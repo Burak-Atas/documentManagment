40fc32461463f36ba083563b8e025b08411d756d
Burak Ataş <batas219@gmail.com>
2024-12-30 00:28:49 +0300

    sudo

--- deneme.js
+++ deneme.js
@@ -7,3 +7,12 @@ console.log('BURAK ATAŞ');
 
 
 console.log("-------------------------------");
+console.log("-------------------------------");
+
+console.log('BURAK ATAŞ');
+console.log('BURAK ATAŞ');
+console.log('yakında maybach alacak');
+console.log('BURAK ATAŞ');
+
+
+console.log("-------------------------------");
