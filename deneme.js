5625cac50e43582ea62aa25fbc07c014646a9213
Burak Ataş <batas219@gmail.com>
2024-12-30 00:30:07 +0300

    sudo

--- deneme.js
+++ deneme.js
@@ -6,8 +6,31 @@ console.log('yakında maybach alacak');
 console.log('BURAK ATAŞ');
 
 
-console.log("-------------------------------");
-console.log("-------------------------------");
+console.log("-------------------------------"); console.log("-------------------------------");
+
+console.log('BURAK ATAŞ');
+console.log('BURAK ATAŞ');
+console.log('yakında maybach alacak');
+console.log('BURAK ATAŞ');
+
+
+console.log("-------------------------------"); console.log("-------------------------------");
+
+console.log('BURAK ATAŞ');
+console.log('BURAK ATAŞ');
+console.log('yakında maybach alacak');
+console.log('BURAK ATAŞ');
+
+
+console.log("-------------------------------"); console.log("-------------------------------");
+
+console.log('BURAK ATAŞ');
+console.log('BURAK ATAŞ');
+console.log('yakında maybach alacak');
+console.log('BURAK ATAŞ');
+
+
+console.log("-------------------------------"); console.log("-------------------------------");
 
 console.log('BURAK ATAŞ');
 console.log('BURAK ATAŞ');
